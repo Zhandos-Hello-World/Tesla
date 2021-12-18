1222a20bab78853c159e02bbe2c8136a33b844f1
jumagaliev1 <200103311@stu.sdu.edu.kz>
2021-12-18 17:17:43 +0600

    Fixed troubles with links

--- res_main/js/script.js
+++ res_main/js/script.js
@@ -8,13 +8,13 @@ function change(element){
 
     switch(i){
         case 0:
-            document.getElementById('bg').style.cssText = 'background-image: url("Roadster.jpg");';
+            document.getElementById('bg').style.cssText = 'background-image: url("res_main/img/Roadster.jpg");';
             document.getElementById("mainWord").innerHTML = "Roadster";
             document.getElementById("mainWord").style.cssText = 'font-family: Japane; color:#A60425FF';
             document.getElementById("roadster").style.cssText = 'display: block';
             break;
         case 1:
-            document.getElementById('bg').style.cssText = 'background-image: url("model-s.png");';
+            document.getElementById('bg').style.cssText = 'background-image: url("res_main/img/model-s.png");';
             document.getElementById("mainWord").innerHTML = "Model S";
             document.getElementById("mainWord").style.cssText = 'font-family: \'Roboto\', sans-serif; color:#181B21';
             for(j = 0; j < document.getElementsByClassName("a").length; j++)
@@ -23,7 +23,7 @@ function change(element){
             document.getElementById("roadster").style.cssText = 'display: none';
             break;
         case 2:
-            document.getElementById('bg').style.cssText = 'background-image: url("model-3.jpeg"); color: white; ';
+            document.getElementById('bg').style.cssText = 'background-image: url("res_main/img/model-3.jpeg"); color: white; ';
             document.getElementById("mainWord").innerHTML = "Model 3";
             document.getElementById("mainWord").style.cssText = 'font-family: \'Roboto\', sans-serif; color:white';
             for(j = 0; j < document.getElementsByClassName("a").length; j++)
@@ -32,7 +32,7 @@ function change(element){
         case 3:
             // document.getElementById("bg").background-image = url("img/img2.png");
             let subTitle = document.getElementById('bg');
-            subTitle.style.cssText = 'background-image: url("model-x.png")';
+            subTitle.style.cssText = 'background-image: url("res_main/img/model-x.png")';
             document.getElementById("mainWord").innerHTML = "Model X";
             document.getElementById("mainWord").style.cssText = 'font-family: \'Roboto\', sans-serif; color:#181B21';
             for(j = 0; j < document.getElementsByClassName("a").length; j++)
@@ -44,7 +44,7 @@ function change(element){
 
 
         case 4:
-            document.getElementById('bg').style.cssText = 'background-image: url("powerwall.jpeg"); color: white; ';
+            document.getElementById('bg').style.cssText = 'background-image: url("res_main/img/powerwall.jpeg"); color: white; ';
             document.getElementById("mainWord").innerHTML = "Powerwall";
             document.getElementById("mainWord").style.cssText = 'font-family: \'Roboto\', sans-serif; color:#181B21';
             break;
