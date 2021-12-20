573712a4dfd7192c2dba90d69783848316be4ab6
Zhandos <200103398@stu.sdu.edu.kz>
2021-12-20 23:59:27 +0600

    logic of crediting

--- res_custom_order/js/Autoize.js
+++ res_custom_order/js/Autoize.js
@@ -1,23 +1,34 @@
 let value = 6;
 
+const dataCosts = Array(
+    Array(87510, 127510),
+    Array(53510, 58510),
+    Array(87510, 127510),
+    Array(39510, 53510)
+)
+
+
+
 const btns = document.querySelectorAll(".space")
 const time = document.getElementById("time")
+const cost = document.getElementById("cost")
+const btnDiv = document.querySelector(".select-car")
+
+const sum1 = document.querySelector(".sum1")
+const sum2 = document.querySelector(".sum2")
+
+
+function selected1() {
+    sum1.innerHTML = dataCosts[btnDiv.selectedIndex][0]
+    sum2.innerHTML = dataCosts[btnDiv.selectedIndex][1]
+}
+
 
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
         value = btns[i].innerText
         time.innerText = "Time in months: " + value
-        //setDefault()
-
-        // btns[i].style.background="rgb(253,29,29)";
-        // btns[i].style.background="linear-gradient(90deg, rgba(253,29,29,1) 45%, rgba(252,176,69,1) 100%)";
     })
 }
-// function setDefault() {
-//     const btns = document.querySelectorAll(".space")
-//     for (let t = 0; t < btns.length; t++) {
-//         btns[t].addEventListener("click", function () {
-//             btns[t].style.background="#F7F7FAFF"
-//         })
-//     }
-// }
+
+
